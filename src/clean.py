1a2f7ae4706c98e9bb3e462aa0fef8970dcf6a23
Agnieszka <106694089+agkittens@users.noreply.github.com>
2024-06-10 13:49:53 +0200

    renamed saving files

--- src/clean.py
+++ src/clean.py
@@ -1,5 +1,7 @@
 import os
 import sys
+
+import pyspark.sql
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, to_date, when, mean
 from pyspark.sql.types import DoubleType, IntegerType, StringType
@@ -7,6 +9,8 @@ from pyspark.sql.types import DoubleType, IntegerType, StringType
 PATHS = None
 
 def create_paths():
+    """ Creates a new path for cleaned csv files """
+
     global PATHS
 
     main_path = "cleaned_data"
@@ -21,6 +25,8 @@ def create_paths():
     PATHS = output_paths
 
 def create_spark_df():
+    """ Spark setup and handling data """
+
     spark = SparkSession.builder \
         .appName("MyApp") \
         .master("local[*]") \
@@ -39,6 +45,8 @@ def create_spark_df():
     spark.stop()
 
 def clean_data(dfs):
+    """ Cleaning data - filling missing values with average temperature """
+
     cols = ["TN", "TG", "TX"]
     labels = ["min", "mean", "max"]
 
@@ -51,16 +59,27 @@ def clean_data(dfs):
         corrected = correct_types(cleaned, idx, cols)
 
         save_data(corrected, labels, idx)
-        # df.write.mode("overwrite").option("header", "true").csv(PATHS[labels[idx]])
 
     for path in PATHS.values():
         for root, dirs, files in os.walk(path):
+
+            # removing additional .crc files
             for file in files:
                 if file.endswith('.crc'):
                     os.remove(os.path.join(root, file))
 
+            # renaming .csv files for more suitable name
+            csv_files = [file for file in files if file.endswith('.csv')]
+            csv_files.sort()
+            for idx,file in enumerate(csv_files):
+                new_filename = f"{idx + 1}.csv"
+                old = os.path.join(root, file)
+                new = os.path.join(root, new_filename)
+                os.rename(old, new)
 
 def correct_types(df,idx: int, cols: list):
+    """ Correcting data types and scaling temperature """
+
     corrected = df.withColumn(cols[idx], (col(cols[idx])/ 10.0).cast(DoubleType())) \
         .withColumn("Q_"+cols[idx], col("Q_"+cols[idx]).cast(IntegerType())) \
         .withColumn("DATE", to_date(col("DATE").cast(StringType()), "yyyyMMdd")) \
@@ -71,7 +90,9 @@ def correct_types(df,idx: int, cols: list):
 
     return corrected
 
-def load_data(spark):
+def load_data(spark: pyspark.sql.SparkSession):
+    """ Loading csv data to dataframes and removing spaces """
+
     main_path = "csv_data"
     input_paths = {
         "min": os.path.join(main_path, "min"),
@@ -92,10 +113,13 @@ def load_data(spark):
     return min_df, mean_df, max_df
 
 
-def save_data(df, labels, idx):
+def save_data(df, labels: list, idx: int):
+    """ Saving cleaned data """
+
     df.write.mode("overwrite").option("header", "true").csv(PATHS[labels[idx]])
 
 
 
-create_paths()
-create_spark_df()
+if __name__ == "__main__":
+    create_paths()
+    create_spark_df()
